47bccc63d6bd08044e47e5fcc46afdbf160d5119
Zeray <tiggreen87@gmail.com>
2018-09-19 10:31:48 +0300

    Added message such as OS, DBMS

--- welcome.c
+++ welcome.c
@@ -5,6 +5,7 @@ int main(int argc,char **argv)
 {
 	printf("Welcome to Git Bash Windows 7 C programming!\n");
 	printf("C programming is the cradle of other programming languages.\n");
+	printf("Masjor platfroms such as OS, DBMS are written in C language.");
 
 	return 0;
 }
