459e29c3f88429b37003ee714cd10329968880d8
Zeray <tiggreen87@gmail.com>
2018-09-19 08:58:35 +0300

    C is cradle of other languages added to welcome.c

--- welcome.c
+++ welcome.c
@@ -3,6 +3,8 @@
 
 int main(int argc,char **argv)
 {
-	printf("Welcome to Git Bash Windows 7 C programming!");
+	printf("Welcome to Git Bash Windows 7 C programming!\n");
+	printf("C programming is the cradle of other programming languages.\n");
+
 	return 0;
 }
